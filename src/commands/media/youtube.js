274c1bb10e37b70f99c957d035d78a847124851e
Deryl <153626742+Push-b@users.noreply.github.com>
2024-08-01 13:54:12 +0200

    Update youtube.js

--- src/commands/media/youtube.js
+++ src/commands/media/youtube.js
@@ -1,4 +1,4 @@
-const YT = require('../../lib/YT2');
+const YT = require('../../lib/YT2')
 const yts = require("youtube-yts");
 
 module.exports = {
@@ -8,7 +8,8 @@ module.exports = {
     description: 'Downloads given YT Video',
     async execute(client, arg, M) {
 
-      const link = async (term) => {
+  
+        const link = async (term) => {
             const { videos } = await yts(term.trim())
             if (!videos || !videos.length) return null
             return videos[0].url
@@ -18,25 +19,26 @@ module.exports = {
         const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
         const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
         if (!term) return M.reply('Please use this command with a valid youtube content link')
-      
-        M.reply(`Downloading: ${linkData?.title}`)
-        if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
-         const audioBuffer = await YT(term, 'video');
+        if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
+         M.reply(`Downloading: ${limkData?.title}`)
+        if (Number(linkData.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
+        const audio = YT.getBuffer(term, 'video')
+            .then(async (res) => {
                 await client.sendMessage(
                     M.from,
                     {
                         video: res,
                         mimetype: 'video/mp4',
-                        fileName: `${linkData.title}.mp4`
-                     },
-                {
-                    quoted: M
-                }
-            );
-       }
-         } catch (error) {
-            console.error(error);
-            M.reply('An error occurred while downloading the YouTube video audio.');
-        }
+                        fileName: videoDetails.title + '.mp4'
+                    },
+                    {
+                        quoted: M
+                    }
+                )
+            })
+            .catch((err) => {
+                return M.reply(err.toString())
+            })
     }
-};
+}
+//M.quoted.mtype === 'imageMessage',
